f321e654f85eef8e15124f4266c1bf8fc0daaf83
Victor Fernandez de Alba <sneridagh@gmail.com>
2016-12-03 14:38:47 +0100

    Polish theme and links pointers

---
+++ src/ploneconf2017/theme/theme/js/theme.js
@@ -0,0 +1,41 @@
+require([
+  'jquery'
+], function($){
+
+  var cycle = function(){
+    $('h1').animate({
+      left: '250px',
+      opacity: '0.5',
+      'font-size': '30px'
+    }, function(){
+      $('h1').animate({
+        left: '0',
+        opacity: '1',
+        'font-size': '20px'
+      }, function(){
+        setTimeout(function(){
+          cycle();
+        }, 2000);
+      });
+    });
+  };
+
+  var smooth = function () {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top - 80
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  };
+
+  $(document).ready(function(){
+    smooth();
+  });
+});
